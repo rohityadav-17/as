c0963a65b804ae0bfd2e9bc8ab24db75ff022295
sdac-23 <sdac-23@sdac-23-PC>
2024-03-16 13:38:49 +0530

    .

--- as/src/as/Emp.java
+++ as/src/as/Emp.java
@@ -2,22 +2,38 @@ package as;
 
 public class Emp {
 
+	
 	@Override
 	public String toString() {
 		return "Emp [Name=" + Name + ", id=" + id + ", sal=" + sal + ", address=" + address + "]";
 	}
-	String Name = "name";
-	int id = 1;
-	double sal = 100;
-	String address = "address"; 
+
+	public Emp(String name, int id, double sal, String address) {
+		super();
+		Name = name;
+		this.id = id;
+		this.sal = sal;
+		this.address = address;
+	}
+	
+	String Name;
+	int id;
+	double sal;
+	String address; 
 
 	public static void main(String[] args) {
-		Emp emp = new Emp(); 
+		Emp emp = new Emp("name", 1, 100, "name");
+		Emp emp2 = new Emp("name", 1, 100, "name");
+		
+		
 		
 		System.out.println(emp.address);
 		System.out.println(emp.id);
-		System.out.println(	emp.Name);
+		System.out.println(emp.Name);
 		System.out.println(emp.sal);
+		
+		emp2.toString();
+		
 	
 	}
 
